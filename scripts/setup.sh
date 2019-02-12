557182e2cd38982479cf5c71c446bdd1fb99fc26
Wi1d5a1m0N <47374678+Wi1d5a1m0N@users.noreply.github.com>
2019-02-12 08:23:24 -0800

    Update setup.sh
    fixed font install issue

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,1 @@
-apt-get -y install figlet && wget http://www.figlet.org/fonts/larry3d.flf -O /usr/share/figlet
+apt-get -y install figlet && wget http://www.figlet.org/fonts/larry3d.flf -O /usr/share/figlet/larry3d.flf
